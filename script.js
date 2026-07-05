a72ea31c41952257db1a69b860d05dcd3ae92cdf
vice <vice@vice.com>
2026-07-05 20:12:36 +0300

    add tiny modal

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ open.addEventListener("click", function () {
 close.addEventListener("click", function () {
   menu.classList.toggle("close");
 });
+
+var button = document.querySelector(".button");
+var modal = document.querySelector(".modal");
+
+// Set will-change when the element is hovered
+button.addEventListener("focus", hintBrowser, true);
+button.addEventListener("blur", hintBrowser, true);
+
+button.addEventListener("click", hintBrowser);
+modal.addEventListener("transitionend", removeHint, false);
+
+function hintBrowser() {
+  // The optimizable properties that are going to change
+  // in the animation's keyframes block
+  modal.style.willChange = "clip-path";
+  console.log("add will-change");
+}
+
+function removeHint() {
+  this.style.willChange = "auto";
+  console.log("remove will-change");
+}
